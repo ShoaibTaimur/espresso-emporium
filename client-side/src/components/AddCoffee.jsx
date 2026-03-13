26839f2b82ae6cf21b1e68ee753315940c6e237f
Shoaib Taimur <mitashoaib@gmail.com>
2026-03-14 01:04:31 +0600

    Added coffee add section

--- client-side/src/components/AddCoffee.jsx
+++ client-side/src/components/AddCoffee.jsx
@@ -1,10 +1,60 @@
 import React from 'react';
 
 const AddCoffee = () => {
+
+    const handleSubmit=e=>{
+        e.preventDefault();
+        const name=e.target.name.value;
+        const supplier=e.target.supplier.value;
+        const category=e.target.category.value;
+        const chef=e.target.chef.value;
+        const taste=e.target.taste.value;
+        const details=e.target.details.value;
+        const photo=e.target.photo.value;
+    }
+
+
+
     return (
         <div>
-            
-        </div>
+            <div className='px-3 sm:px-15 lg:px-28 py-17.5 bg-[#F4F3F0] space-y-5'>
+                <div className='flex flex-col items-center gap-5'>
+                    <h1 className="rancho text-[26px] md:text-[35px] text-shadow-xl font-normal text-[#374151]">
+                        Add Coffee
+                    </h1>
+                    <p className='raleway opacity-40 text-center px-10 lg:px-20.5 text-[12px]'>
+                        It is a long established fact that a reader will be distraceted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here.
+                    </p>
+                </div>
+                <form onSubmit={handleSubmit} className='md:grid md:grid-cols-2 gap-5'>
+                    <div className='flex flex-col gap-3'>
+                        <label className="label font-semibold">Name</label>
+                        <input name='name' type="text" className="input w-full border-none" placeholder="Enter coffee name" />
+
+                        <label className="label font-semibold">Supplier</label>
+                        <input name='supplier' type="text" className="input border-none w-full" placeholder="Enter coffee supplier" />
+
+                        <label className="label font-semibold">Category</label>
+                        <input name='category' type="text" className="input border-none w-full" placeholder="Enter coffee category" />
+                    </div>
+                    <div className='flex flex-col gap-3'>
+                        <label className="label font-semibold">Chef</label>
+                        <input name='chef' type="text" className="input border-none w-full" placeholder="Enter coffee chef" />
+
+                        <label className="label font-semibold">Taste</label>
+                        <input name='taste' type="text" className="input border-none w-full" placeholder="Enter coffee taste" />
+
+                        <label className="label font-semibold">Details</label>
+                        <input name='details' type="text" className="input border-none w-full" placeholder="Enter coffee details" />
+                    </div>
+                    <div className='col-span-2 flex flex-col gap-3'>
+                        <label className="label font-semibold">Photo</label>
+                        <input name='photo' type="text" className="input border-none w-full" placeholder="Enter Photo URL" />
+                    </div>
+                    <input type="submit" className="col-span-2 rancho mt-3 text-[20px] input w-full bg-[#D2B48C]" value="Add Coffee"/>
+                </form>
+            </div>
+        </div >
     );
 };
 
--- client-side/src/components/Header.jsx
+++ client-side/src/components/Header.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import logo from "../../../assets/more/logo1.png";
 
 const Header = () => {
     return (
-        <div>
-            
+        <div className='bg-[#331A15] py-3.75 flex justify-center'>
+            <div className='flex items-center'>
+                <img className='w-12 md:w-18.75' src={logo} alt="" />
+                <h1 className='rancho text-white text-[22px] md:text-[40px]'>Espresso Emporium</h1>
+            </div>
         </div>
     );
 };
